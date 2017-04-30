a728e58d3a4cb33d853aee3cb3f5eb28ffcd1201
ggzucco <ggzucco@gmail.com>
2017-04-29 23:19:48 -0300

    Add Archives
    Add Archives

---
+++ LM35.cpp
@@ -0,0 +1,39 @@
+/*    READ FIRST 
+ * 
+ * LM35 Library 
+ * LM35.CPP
+ * VERSION: 1.0
+ * 
+ * Author: Guilherme Gomes Zucco
+ * 
+ * This library set the pin where is the lm35 sensor,
+ * and converts the raw value to Celsius or Fahrenheit or Kelvin.
+ * 
+*/
+
+#include "Arduino.h"
+#include "LM35.h"
+
+LM35::LM35(int pin)
+{
+    pinMode(pin, OUTPUT);
+    _pin = pin;
+}
+
+float LM35::cel()
+{
+   float tempC = (float(analogRead(_pin))*5/(1023))/0.01;
+   return tempC;
+}
+
+float LM35::fah()
+{
+   float tempF = (((float(analogRead(_pin))*5/(1023))/0.01)*1.8)+32;
+   return tempF;
+}
+
+float LM35::kel()
+{
+   float tempK = ((float(analogRead(_pin))*5/(1023))/0.01)+273;
+   return tempK;
+}
---
+++ LM35.h
@@ -0,0 +1,32 @@
+/*    READ FIRST 
+ * 
+ * LM35 Library 
+ * LM35.H
+ * VERSION: 1.0
+ * 
+ * Author: Guilherme Gomes Zucco
+ * 
+ * This library set the pin where is the lm35 sensor,
+ * and converts the raw value to Celsius or Fahrenheit or Kelvin.
+*/
+
+#ifndef LM35_h
+#define LM35_h
+#include "Arduino.h"
+
+
+class LM35
+{
+   public:
+       LM35(int pin);
+       float cel();
+       float fah();
+	   float kel();
+
+   private:
+       int _pin;
+
+};
+
+
+#endif
